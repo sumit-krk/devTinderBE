70f8c3dd654746ae0b55a76c3889b0dcdde8d1d3
pico-8to <sumitkumar308786@gmail.com>
2026-04-29 11:20:36 +0530

    created express server

---
+++ src/app.js
@@ -0,0 +1,10 @@
+const express=require("express")
+const app = express();
+
+app.use("/test",(req, res)=>{
+    res.send("Hello from server")
+})
+
+app.listen(3000, ()=>{
+    console.log("Server is running on port 3000")
+})
